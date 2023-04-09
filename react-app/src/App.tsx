c284f897f0e9e816889527513a99bda0f21a670d
MagicSakuraD <ckgoforit@outlook.com>
2023-04-09 23:30:52 +0800

    some error

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,11 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const name = "Mosh";
-  if (name) {
-    return <h1>Hello {name}</h1>;
-  }
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;
