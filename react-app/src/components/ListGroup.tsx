9e33a471bdead95dab93383f7ce43892825cf0ea
MagicSakuraD <ckgoforit@outlook.com>
2023-04-10 22:54:10 +0800

    add event Clickhandle

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  return <h1>List Group</h1>;
+  const items = ["New York", "London", "San Francisco", "Tokyo", "Paris"];
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
