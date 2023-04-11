26c320c03df6f652af027ad574789191edca891c
MagicSakuraD <ckgoforit@outlook.com>
2023-04-11 11:06:26 +0800

    add sate

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,27 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { MouseEvent } from "react";
 
 function ListGroup() {
   const items = ["New York", "London", "San Francisco", "Tokyo", "Paris"];
-  const handleClick = (event: MouseEvent) => console.log(event);
 
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
